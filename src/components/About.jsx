76ae202edde94c7afcfa7f8e3b329be143567ae0
kt <cjtakhar@gmail.com>
2023-09-19 16:14:36 -0700

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,15 @@
+import Image from '../images/community.png'
 import '../styles/About.css'
 
 const About = () => {
     return (
         <div className="about-container">
             <div className="bio-container">
-                <h1>About Us</h1>
-                <p>We are Truckee community members who want to make sharing and displaying art accessible for everyone.</p>
+                <h1 className="about-header">About Us</h1>
+                <p classname="about-paragraph">We are Truckee community members who want to make sharing and displaying art accessible for everyone.</p>
+            </div>
+            <div className="community-img-container">
+                <img src={Image} alt="community" className="community-img" />
             </div>
         </div>
     )
